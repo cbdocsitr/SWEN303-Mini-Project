ca0576b4b78b3c377a2cd58633b7e37cf38a6e4d
Iris Riddiford Graham <irisjo.rg@gmail.com>
2016-03-30 00:57:49 +1300

    With browse functioning

--- routes/index.js
+++ routes/index.js
@@ -13,14 +13,7 @@ var basexQuery = "XQUERY declare default element namespace 'http://www.tei-c.org
 
 /* GET home page. */
 router.get("/",function(req,res){
-client.execute(basexQuery + " (//name[@type='place'])[1] ",
-function (error, result) {
-	if(error){ console.error(error);}
-	else {
-		res.render('index', { title: 'Colenso Databse', place: result.result });
-	}
-		}
-		);
+	res.render('index', { title: 'Colenso Database'});
 });
 
 //SEARCH TEXT
@@ -28,16 +21,22 @@ router.get('/search-text', function(req, res) {
 if(req.query.searchString){
 	var queryArray = req.query.searchString.split(" ");
 	var queryString = "";
-	var contains1 = "//*[. contains text '";
+	var contains1 = "//TEI[. contains text '";
 	var contains2 = "']";
 	var wildcard = false;
+	var firstElement = queryArray[0];
+	var firstElementArray = firstElement.split("");
+	var lastChar = firstElementArray[firstElementArray.length-1];
 	var i = 0;
-	if(queryArray[i]==="NOT"){
+	if(firstElement==="NOT"){
 		contains1 = "//*[not(. contains text '";
 		contains2 = "')]";
 		++i;
-	}else if(queryArray[i]==="*"){
+	}else if(lastChar==="*"){
+		queryArray[0].replace("*", "");
 		wildcard = true;
+		contains1 = "//*[starts-with(., '";
+		contains2 = "')]";
 	}
 	while(i < queryArray.length){
 		queryString += queryArray[i];
@@ -55,7 +54,7 @@ if(req.query.searchString){
 	}
 }
 if(wildcard){
-	contains1 = "//*[.]";
+	contains1 = "//TEI[.]";
 	queryString = "";
 	contains2 = "";
 }
@@ -66,7 +65,7 @@ function (error, result) {
 	if(error){ console.error(error)}
 	else {
 		if(req.query.searchString == undefined || req.query.searchString == null){
-			res.render('search-text', { title: 'Colenso Databse', results: " "});
+			res.render('search-text', { title: 'Colenso Database', results: " "});
 		}else{
 			$  = cheerio.load(result.result, {
 				xmlMode: true
@@ -88,53 +87,62 @@ function (error, result) {
 
 //SEARCH MARKUP
 router.get('/search-markup', function(req, res) {
-	client.execute(basexQuery + req.query.searchString, function (error, result) {
-		var list = [];
-		var results = result.result.split("\n");
-		client.execute(basexQuery + "for $n in (collection('Colenso/')" + req.query.searchString + ")\n" +
-		"return db:path($n)", function (error2, resultDB) {
-			var paths = resultDB.result.split("\n");
-			for(i = 0; i <paths.length; i++){
-				var id = paths[i].replace(".xml", "");
-				id = id.split("/")[2];
-				if(id[0]==="N"){
-					id = "Colenso-"+id;
+	if(req.query.searchString == undefined || req.query.searchString == null){
+		res.render('search-markup', { title: 'Colenso Database', results: " "});
+	}else{
+		client.execute(basexQuery + req.query.searchString, function (error, result) {
+			var list = [];
+			var results = result.result.split("\n");
+			client.execute(basexQuery + "for $n in (collection('Colenso/')" + req.query.searchString + ")\n" +
+			"return db:path($n)", function (error2, resultDB) {
+				var paths = resultDB.result.split("\n");
+				for(i = 0; i <paths.length; i++){
+					var id = paths[i].replace(".xml", "");
+					id = id.split("/")[2];
+					if(id[0]==="N"){
+						id = "Colenso-"+id;
+					}
+					var title = results[i];
+					list.push({title: title, href: "/document/" + id});
 				}
-				console.log(i+": "+id);
-				var title = results[i];
-				list.push({title: title, href: "/document/" + id});
-			}
-			res.render('search-markup', { visited: true, number: list.length, results: list});
+				res.render('search-markup', { visited: true, number: list.length, results: list});
+			});
 		});
-	});
+	}
 });
 
 router.get("/browse",function(req,res){
 	res.render('browse', { title: "Colenso Database"});
 });
 
+//author display
 router.get("/browse-author/:author",function(req,res){
-	client.execute(basexQuery + "(//author[@person= '" + req.params.id + "' ])",
+	var authorArray = req.params.author .split("-");
+	var authorQuery = "";
+	for(i = 0; i < authorArray.length; i++){
+		var nextWord = authorArray[i];
+		nextWord[0] = nextWord[0].toUpperCase();
+		authorQuery = authorQuery + " " + nextWord;
+	}
+	client.execute(basexQuery + "(//TEI[. contains text '" + authorQuery +"' ])", 
 		function (error, result) {
+			var list = [];
 		if(error){ console.error(error)}
 		else{
-			var $ = cheerio.load(result.result, {
+			$  = cheerio.load(result.result, {
 				xmlMode: true
 			});
-			var title = $("title").first().text();
-			var author = $("author").first().text();
-			var body = $("body").first().html();
-			var templateData = { 
-				name: title,
-				author: author,
-				body: body,
-				href: "/download/" + req.params.id
-			};
-			res.render('browse', { templateData});
+			$("TEI").each(function(i, elem){
+				var title = $(elem).find("title").first().text();
+				var id = $(elem).attr("xml:id");
+				if(!id){
+					return;
+				}
+				list.push({title: title, href: "/document/" + id});
+			});
+			res.render('browse', { visited: true, results: list});		
 		}
 	});
-	
-	res.render('browse', { title: "Colenso Database"});
 });
 
 //Document display route
